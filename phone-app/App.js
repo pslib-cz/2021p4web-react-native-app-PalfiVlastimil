afd43b6abe79626995030e2b4cdab022c1125d6d
PalfiVlastimil <57211432+PalfiVlastimil@users.noreply.github.com>
2022-04-13 15:03:24 +0200

    basic menu, Contacts API still doesnt work

--- phone-app/App.js
+++ phone-app/App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { NavigationContainer, StackActions } from '@react-navigation/native'
+
 import { NativeScreenContainer } from 'react-native-screens';
 import Contacts from './screens/Contacts'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar, faPhone, faUser, faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
+import { Appbar } from 'react-native-paper'
+
 import Favorites from './screens/Favorites';
 import DirectCall from './screens/DirectCall';
-
 export const FAVORITES = "Favorites";
 export const DIRECT_CALL = "Direct Call";
 export const CONTACTS = "Contacts";
@@ -14,22 +18,55 @@ export const CONTACTS = "Contacts";
 export default function App() {
 
   const Tab = createBottomTabNavigator();
+  //navigace => appbar
+ 
+
   // ve středu test z MIT
   return (
-    <SafeAreaView>
+    
 
       <NavigationContainer>
+        
         <Tab.Navigator
           initialRouteName={CONTACTS}
-        >
-          <Tab.Screen name={FAVORITES} component={Favorites}/>
+          
+          screenOptions={({route}) => ({
+           headerRight: () => (
+             <View styles={styles.iconContainer}>
+               <FontAwesomeIcon style={{paddingRight: 30}} icon={faEllipsisVertical}/>
+             </View>
+           ),
+            tabBarIcon: 
+            ({focused, color, size}) =>{
+              let iconName;
+              switch(route.name){
+                case FAVORITES:{
+                  iconName = faStar;
+                  break;
+                }
+                case DIRECT_CALL:{
+                  iconName = faPhone;
+                  break;  
+                }
+                case CONTACTS:{
+                  iconName = faUser;
+                  break;
+                }
+              }
+              return <FontAwesomeIcon icon={iconName} />
+            }
+          
+          })}
+          >
+          <Tab.Screen name={FAVORITES} options={{headerTitle: 'hellp'}} component={Favorites}/>
           <Tab.Screen name={DIRECT_CALL} component={DirectCall}/>
           <Tab.Screen name={CONTACTS} component={Contacts}/>
         </Tab.Navigator>
 
 
       </NavigationContainer>
-    </SafeAreaView>
+        
+    
   );
 }
 
@@ -40,4 +77,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  iconContainer:{
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: 120,
+  },
+  });
--- phone-app/screens/Contacts.jsx
+++ phone-app/screens/Contacts.jsx
@@ -1,16 +1,46 @@
 import React from 'react';
 import { Text, View, SafeAreaView } from 'react-native';
+import { Button } from 'react-native-web';
+import { PermissionsAndroid } from 'react-native-web';
+import {Contacts as Cont  }from 'react-native-contacts';
+import { DIRECT_CALL } from '../App';
 
-const Contacts = () => {
+
+PermissionsAndroid.request(
+  PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+  {
+    'title': 'Contacts',
+    'message': 'This app would like to view your contacts.',
+    'buttonPositive': 'Please accept bare mortal'
+  }
+)
+  .then(Cont.getAll()
+    .then((contacts) => {
+        // work with contacts
+          console.log(contacts)
+        })
+          .catch((e) => {
+              console.log(e)
+          }))
+
+
+const Contacts = ({navigation}) => {
+  
     return (
-        <View
+        <SafeAreaView
         style={{
           flex: 1,
           justifyContent: "center",
           alignItems: "center"
         }}>
         <Text>Hello, world!</Text>
-      </View>
+        <Button
+        title="what the f..." 
+        onPress={() => navigation.navigate(DIRECT_CALL)}
+        />
+
+        
+      </SafeAreaView>
     )
 }
 export default Contacts;
